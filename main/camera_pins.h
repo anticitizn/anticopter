c96f6c703c4095d239c8390116186223acc90572
D. Neykov <metheorita@gmail.com>
2024-04-08 23:05:26 +0200

    initial commit

---
+++ main/camera_pins.h
@@ -0,0 +1,18 @@
+
+#define CAM_PIN_PWDN -1  //power down is not used
+#define CAM_PIN_RESET -1 // software reset will be performed
+#define CAM_PIN_XCLK 13
+#define CAM_PIN_SIOD 45
+#define CAM_PIN_SIOC 48
+
+#define CAM_PIN_D9 14
+#define CAM_PIN_D8 12
+#define CAM_PIN_D7 11
+#define CAM_PIN_D6 9
+#define CAM_PIN_D5 3
+#define CAM_PIN_D4 18
+#define CAM_PIN_D3 8
+#define CAM_PIN_D2 46
+#define CAM_PIN_VSYNC 47
+#define CAM_PIN_HREF 21
+#define CAM_PIN_PCLK 10
